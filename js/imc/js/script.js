835e855e7d14c2610be09824eb1fc094996f3e8f
Isaac Gonzales Vizcarra <isaacgonzalesv@gmail.com>
2024-05-23 11:32:00 +0200

    add project calculate imc

---
+++ js/imc/js/script.js
@@ -0,0 +1,36 @@
+import { Modal } from './modal.js';
+import { AlertError } from './alert-error.js';
+import { IMC, notNumber } from './utils.js';
+
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+inputWeight.oninput = () => AlertError.close();
+inputHeight.oninput = () => AlertError.close();
+
+form.onsubmit = event => {
+    event.preventDefault();
+
+    const weight = inputWeight.value;
+    const height = inputHeight.value;
+
+    const showAlertError = notNumber(weight) || notNumber(height);
+
+    if (showAlertError) {
+        AlertError.open();
+        return;
+    }
+
+    AlertError.close();
+
+    const result = calculateIMC(weight, height);
+    displayResultMessage(result);
+}
+
+function displayResultMessage(result) {
+    const message = `Your IMC is ${result}`;
+    Modal.message.innerText = message;
+    Modal.open();
+}
+
