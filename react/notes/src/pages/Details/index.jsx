345f5a616ecc0ae01e2a72ed5933bd5b97c7ce59
Isaac Gonzales Vizcarra <isaacgonzalesv@gmail.com>
2024-06-10 14:59:12 +0200

    add content

--- react/notes/src/pages/Details/index.jsx
+++ react/notes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container,  Links} from './styles';
+import { Container,  Links, Content} from './styles';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -7,24 +7,34 @@ import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
 import { ButtonText } from '../../components/ButtonText';
 
+
 export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Remove note" />
-      <Section title="Links">
-        <Links>
-          <li><a href="#">https://</a></li>
-          <li><a href="#">https://</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+        <ButtonText title="Remove note" />
+        <h1>React</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam doloribus quod temporibus corporis modi placeat tempore voluptatibus nulla repellendus maxime veritatis ex eaque, id quisquam cumque, dolor nostrum quae fugit?
+        </p>
+        <Section title="Links">
+          <Links>
+            <li><a href="#">https://</a></li>
+            <li><a href="#">https://</a></li>
+          </Links>
+        </Section>
 
-      <Section title="Tags">
-          <Tag title="express"/>
-          <Tag title="nodejs"/>
+        <Section title="Tags">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
 
-      </Section>
-      <Button title="Return" loading/>
+        </Section>
+        <Button title="Return" loading/>
+
+      </Content>
+      </main>
     </Container>
   )
 }
