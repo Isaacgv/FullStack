a08831871131672f99a58b17920515559147fe32
Isaac Gonzales Vizcarra <isaacgonzalesv@gmail.com>
2024-06-10 13:42:25 +0200

    add button text

--- react/notes/src/pages/Details/index.jsx
+++ react/notes/src/pages/Details/index.jsx
@@ -2,14 +2,16 @@ import { Container,  Links} from './styles';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
-import { Section } from '../../components/Section/styles';
+import { Section } from '../../components/Section';
 
 import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Remove note" />
       <Section title="Links">
         <Links>
           <li><a href="#">https://</a></li>
