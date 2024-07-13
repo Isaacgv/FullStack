bedd8ed6f3513ea84cb2b3009c32b7b87e22c643
Isaac Gonzales Vizcarra <isaacgonzalesv@gmail.com>
2024-07-13 15:07:32 +0200

    add profile

---
+++ react/notes/src/pages/Profile/index.jsx
@@ -0,0 +1,45 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock} from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form } from "./styles";
+
+export function Profile() {
+    return (
+        <Container> 
+            <header>
+                <a href="/">
+                    <FiArrowLeft />
+                </a>
+            </header>
+
+            <Form>
+                <Input 
+                    placeholder="Name"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input 
+                    placeholder="Email"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Input 
+                    placeholder="New password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Save"/>
+            </Form>
+
+        </Container>   
+    )
+}
